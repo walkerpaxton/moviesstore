9e97f39e1e3aa08e0c91753ecfac6578b5e1339d
walkerpaxton <walkpax@gmail.com>
2025-09-15 22:22:22 -0400

    switch to sqllite

--- movies/models.py
+++ movies/models.py
@@ -20,4 +20,9 @@ class Review(models.Model):
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
 
+class Like(models.Model):
+    id = models.AutoField(primary_key=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 ##if this is changed or updated, then need to run python3 manage.py makemigrations and then python3 manage.py migrate again
