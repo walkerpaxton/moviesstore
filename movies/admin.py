d71006e213e8d5f1d7489158404531715067a5a7
walkerpaxton <walkpax@gmail.com>
2025-09-15 23:12:02 -0400

    added like/dislike system

--- movies/admin.py
+++ movies/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Movie, Review
+from .models import Movie, Review, Like
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name'] 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
+admin.site.register(Like)
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Movie, Review
+from .models import Movie, Review, Like
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -16,10 +16,16 @@ def index(request):
 def show(request, id):
     movie = Movie.objects.get(id=id)
     reviews = Review.objects.filter(movie=movie)
+    like_count = Like.objects.filter(movie=movie).count()
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
     template_data['reviews'] = reviews
+    template_data['like_count'] = like_count
+    user_has_liked = False
+    if request.user.is_authenticated:
+        user_has_liked = Like.objects.filter(movie=movie, user=request.user).exists()
+    template_data['user_has_liked'] = user_has_liked
     return render(request, 'movies/show.html', {'template_data': template_data})
 
 @login_required
@@ -58,3 +64,24 @@ def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
     return redirect('movies.show', id=id)
+
+@login_required
+def like_movie(request, id):
+    movie = Movie.objects.get(id=id)
+    like_count = Like.objects.filter(movie=movie).count()
+    
+    if request.method == 'POST':
+        existing_like = Like.objects.filter(movie=movie, user=request.user).first()
+        if existing_like:
+            existing_like.delete()
+        else:
+            like = Like()
+            like.movie = movie
+            like.user = request.user
+            like.save()
+        return redirect('movies.show', id=id)
+    else:
+        template_data = {}
+        template_data['title'] = 'Like Movie'
+        template_data['like_count'] = like_count
+        return render(request, 'movies/like_button.html', {'template_data': template_data})
